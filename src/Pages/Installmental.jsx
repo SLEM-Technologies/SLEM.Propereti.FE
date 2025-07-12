7edb394e5e022ccfadbdac6693cae4725f09ea78
ddanny16 <102356223+ddanny16@users.noreply.github.com>
2025-07-12 20:52:40 +0100

    ...

---
+++ src/Pages/Installmental.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Horheader from "../Components/Horheader";
+
+
+const Installmental = () => {
+  return (
+    <div className={styles.Installmental}>
+      <Horheader />
+    </div>
+  );
+};
+
+export default Installmental;
